6daa0f6d791bcc2189b3c765639b0c41330e40c1
Martion <strani@163.com>
2017-08-03 21:24:07 +0800

    Initial commit

---
+++ src/main/java/com/Martion/Doc4h/Core/Doc4hCore.java
@@ -0,0 +1,23 @@
+package com.Martion.Doc4h.Core;
+
+/**
+ * 核心日志生产类，主函数
+ */
+public class Doc4hCore {
+
+    /**
+     * <1>初始化配置文件
+     * @param Path
+     * @return
+     */
+    public Doc4hCore setSourcePath(String Path){
+        return this;
+    }
+
+    public void buildDoc(){
+
+
+    }
+
+
+}
---
+++ src/main/java/com/Martion/Doc4h/Util/PropertiesContent.java
@@ -0,0 +1,13 @@
+package com.Martion.Doc4h.Util;
+
+import java.util.Map;
+
+public class PropertiesContent {
+
+    public static Map<String,String> properties = null;
+
+    public static void getProtertiesByPath(String path){
+
+
+    }
+}
