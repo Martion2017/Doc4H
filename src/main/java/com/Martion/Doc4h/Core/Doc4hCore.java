bda2ea3b261d4232cf9c77b67c84547eeb78ee4e
Martion <strani@163.com>
2017-08-07 11:14:25 +0800

    增加读取配置文件的util
    初始化配置文件，读取配置属性

--- src/main/java/com/Martion/Doc4h/Core/Doc4hCore.java
+++ src/main/java/com/Martion/Doc4h/Core/Doc4hCore.java
@@ -1,9 +1,14 @@
 package com.Martion.Doc4h.Core;
 
+import com.Martion.Doc4h.Util.PropertiesContent;
+
+import java.util.logging.Logger;
+
 /**
  * 核心日志生产类，主函数
  */
 public class Doc4hCore {
+
 
     /**
      * <1>初始化配置文件
@@ -11,12 +16,25 @@ public class Doc4hCore {
      * @return
      */
     public Doc4hCore setSourcePath(String Path){
+        if(Path!=null&&!"".equals(Path)){
+            try {
+                PropertiesContent.getProtertiesByPath(Path);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }else{
+
+        }
+
         return this;
     }
 
     public void buildDoc(){
+        
 
-
+    }
+    public static void main(String args[]){
+        new Doc4hCore().setSourcePath("doc4h.properties");
     }
 
 
--- src/main/java/com/Martion/Doc4h/Util/PropertiesContent.java
+++ src/main/java/com/Martion/Doc4h/Util/PropertiesContent.java
@@ -1,13 +1,57 @@
 package com.Martion.Doc4h.Util;
 
+import org.springframework.core.io.support.PropertiesLoaderUtils;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Map;
+import java.util.Properties;
+import java.util.ResourceBundle;
+
 
 public class PropertiesContent {
 
-    public static Map<String,String> properties = null;
 
-    public static void getProtertiesByPath(String path){
+    private static Properties propertie = new Properties();
 
+
+    /**
+     * 初始化配置文件
+     * @param path(如：doc4h.properties)
+     * @throws Exception
+     */
+    public static void getProtertiesByPath(String path) throws Exception {
+        try {
+            propertie = PropertiesLoaderUtils.loadAllProperties(path);
+        } catch (FileNotFoundException ex) {
+            throw new Exception(path+".properties文件不存在！");
+
+        } catch (IOException ex) {
+            throw new Exception(path+".properties文件读取错误！");
+
+        }catch(NullPointerException ex){
+            throw new Exception(path+".properties文件名为空！");
+
+        }
+
+    }
+
+    /**
+     * 根据key值获取配置文件中的属性
+     * @param key
+     * @return value
+     * @throws Exception
+     */
+    public static String getProperties(String key) throws Exception {
+        if(propertie.isEmpty()){
+            throw new Exception("请先初始化配置！");
+        }
+        if(propertie.containsKey(key)){
+            return propertie.getProperty(key);
+        }else{
+            return null;
+        }
 
     }
 }
